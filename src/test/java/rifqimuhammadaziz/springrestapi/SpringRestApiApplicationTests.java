4eba2256530f3c682d656f8fad982f3cb3c26568
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-05-16 07:08:37 +0700

    Create Spring Project

---
+++ src/test/java/rifqimuhammadaziz/springrestapi/SpringRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package rifqimuhammadaziz.springrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
